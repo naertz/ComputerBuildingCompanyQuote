e48dbc4909ef3f361a33abd44a10b982405d3325
nae <nae@NAE-AGD0-GA000-DMACC.localdomain>
2021-11-09 23:40:37 -0600

    Initial commit

---
+++ src/main/java/computerbuilding/ComputerBuildingCompanyQuotesApplication.java
@@ -0,0 +1,13 @@
+package computerbuilding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerBuildingCompanyQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerBuildingCompanyQuotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/computerbuilding/ComputerBuildingCompanyQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package computerbuilding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerBuildingCompanyQuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
